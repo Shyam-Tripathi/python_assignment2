09efaae0270ea5eede78f596addf3927a8e386af
Shyam-Tripathi <shyamtripathi2003@gmail.com>
2025-08-29 00:00:11 +0530

    uodate

---
+++ task 1.py
@@ -0,0 +1,9 @@
+# task 1
+# Take input from the user.
+num = int(input("Enter an integer: "))
+
+# Check if the number is even or odd
+if num % 2 == 0:
+    print("number is even.")
+else:
+    print("number is odd.")
---
+++ task 2.py
@@ -0,0 +1,9 @@
+#task 2
+sum = 0
+
+#loop to iterate from 1 to 50
+for number in range(1, 51):
+    sum = sum+number
+
+#final sum
+print("The sum of integers from 1 to 50 is:",sum)
